e62b2e09acd774a0c384b1f3e6fb604e5b888e79
hrome <58020216+hromero7@users.noreply.github.com>
2020-06-21 20:11:11 -0700

    adding offline functionality

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+    entry: "./public/index.js",
+    output: {
+        path: __dirname + "/public/dist",
+        filename: "bundle.js"
+    },
+    mode: "production",
+    plugins: [
+        new WebpackPwaManifest({
+            filename: "manifest.json",
+            inject: false,
+            fingerprints: false,
+
+            name: "Budget Tracker",
+            short_name: "Budget Tracker",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+
+            icons: [
+                {
+                    src: path.resolve(
+                        __dirname, "public/icons/512x512.png"
+                    ),
+                    size: [192, 512]
+                }
+            ]
+        })
+    ]
+};
+
+module.exports = config;
